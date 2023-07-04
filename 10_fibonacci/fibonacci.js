c9b6ad931a44286602a1c28c844db4ea88d0cc0c
hquanvo <vo.hoang.quan2003@gmail.com>
2023-07-04 18:00:14 +0700

    Finish all js exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(val) {
+    let processed = 0;
+    if (typeof val === 'string') processed = parseInt(val);
+    else processed = val;
+    if (processed < 0) return "OOPS";
+    if (processed === 1 || processed === 2) return 1;
+    else return fibonacci(processed-1) + fibonacci(processed-2);
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    oldestAge = 0;
+    oldexIndex = 0;
+    for (let i = 0; i < people.length; i++) {
+        let person = people[i];
+        if (!("yearOfDeath" in person)) {
+            person["yearOfDeath"] = new Date().getFullYear();
+        }
+        let age = person["yearOfDeath"] - person["yearOfBirth"]; 
+        if (age > oldestAge) {
+            oldestAge = age;
+            oldexIndex = i;
+        }
+    }
+    return people[oldexIndex];
 };
 
 // Do not edit below this line
